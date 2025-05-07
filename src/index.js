5cceb0afa1f652b55347a82c32671dfd20c6b801
sanketm710 <sanketm710@gmail.com>
2025-05-08 00:24:47 +0530

    latest change

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./me1.jpg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Portfolio Website" />
-        <link rel="manifest" href="public\manifest.json" />
-        <title>Sanket's Portfolio</title>
-      </head>
-      <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <div id="root"></div>
-      </body>
-    </html>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
