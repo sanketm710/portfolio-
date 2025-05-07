a377da37fed480216cab160a509b6d72c294448b
sanketm710 <sanketm710@gmail.com>
2025-05-07 20:52:01 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./me1.jpg" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./me1.jpg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Portfolio Website" />
-        <link rel="manifest" href="public\manifest.json" />
-        <title>Sanket's Portfolio</title>
-      </head>
-      <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <div id="root"></div>
-      </body>
-    </html>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
